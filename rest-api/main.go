d631ae82855f9ab5cb609b9b1f9104d73e1eb39b
alrafi <hafisalrafi@gmail.com>
2020-10-13 21:41:04 +0700

    init router and endpoints

---
+++ rest-api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+	"encoding/json"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// init router
+	r := mux.NewRouter()
+
+	// route handlers (endpoints)
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
