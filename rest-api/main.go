e1ad6782e1adb533953934f8ab2fef3cdd84d5e3
alrafi <hafisalrafi@gmail.com>
2020-10-13 21:46:12 +0700

    add book struct

--- rest-api/main.go
+++ rest-api/main.go
@@ -10,6 +10,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Book struct (Model)
+type Book struct {
+	ID			string	`json:"id"`
+	Isbn		string	`json:"isbn"`
+	Title		string	`json:"title"`
+	Author	*Author	`json:"author"`
+}
+
 func main() {
 	// init router
 	r := mux.NewRouter()
