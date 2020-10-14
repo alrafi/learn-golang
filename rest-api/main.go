1cc6061483928c132534d4fb54bdf15648eb4a1f
alrafi <hafisalrafi@gmail.com>
2020-10-14 16:44:53 +0700

    impelement create a book method

--- rest-api/main.go
+++ rest-api/main.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"encoding/json"
 	"net/http"
-	// "math/rand"
-	// "strconv"
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -48,7 +48,12 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // add a book
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // update a book
