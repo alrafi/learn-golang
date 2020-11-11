9dc305ef4a695a76bc350530b80e75cb57d2e5e5
TAPTALK-ASUSX40\TapTalk.io <hafis@taptalk.io>
2020-11-11 10:07:40 +0700

    add variable

---
+++ basic/basic_5_variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	a = "Halo"
+
+	var b = "Alrafi"
+	b = "Irsal"
+
+	var b2 = 24 // tipe b2 yaitu int (kalo int berarti mengikuti os => jadi int32 atau int64)
+	fmt.Println(b2)
+
+	c := "Indonesia" 
+	c = "Singapore"
+
+	c2 := 12 // tipe c2 int
+	fmt.Println(c2)
+
+	fmt.Println(a, b, c)
+}
